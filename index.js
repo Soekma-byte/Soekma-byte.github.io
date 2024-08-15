116302ee6bd80f7b39562aa691b8f0fe3599175e
Sukma Ramanda <ramanda.sukma21@gmail.com>
2024-08-15 21:25:06 +0700

    create index

--- index.js
+++ index.js
@@ -1,20 +1,61 @@
-var calcDisplay = ""
-
-function btnClick (e){
-    if (e == "1" || e == "2" || e == "3" || e == "4" || e == "/" || e == "*")
-        if (e == "AC") {
-        calcDisplay =""
-    } else {
-    calcDisplay = calcDisplay + e
-    console.log(calcDisplay)
-    document.getElementById("display").value = calcDisplay
-    } else {
-    console.log("Input harus berupa angka dan simbol")
+function calculateBMI() {
+    var heightInput = document.getElementById("height");
+    var weightInput = document.getElementById("weight");
+    var resultDiv = document.getElementById("result");
+}
+    var height = parseFloat(heightInput.value);
+    var weight = parseFloat(weightInput.value);
+  
+    if (isNaN(height) || isNaN(weight)) {
+      resultDiv.innerHTML = "Please enter valid height and weight.";
+      return;
     }
 
-
+    // Validasi jenis kelamin
+  if (gender === '' || !['Pria', 'Wanita'].includes(gender)) {
+    genderErrorMessage.innerText = 'Pilih jenis kelamin terlebih dahulu';
+    isValid = false;
+  
+    var bmi = weight / ((height / 100) ** 2);
+    var category = "";
+  
+    if (bmi < 18.5) {
+      category = "Underweight";
+    } else if (bmi < 25) {
+      category = "Normal weight";
+    } else if (bmi < 30) {
+      category = "Overweight";
+    } else {
+      category = "Obese";
+    }
+  
+    resultDiv.innerHTML = "Your BMI is " + bmi.toFixed(2) + " (" + category + ")";
+  }
+  
+  function calculateBMI() {
+    var heightInput = document.getElementById("height");
+    var weightInput = document.getElementById("weight");
+    var resultDiv = document.getElementById("result");
+  
+    var height = parseFloat(heightInput.value);
+    var weight = parseFloat(weightInput.value);
+  
+    if (isNaN(height) || isNaN(weight)) {
+      resultDiv.innerHTML = "Please enter valid height and weight.";
+      return;
+    }
+  
+    var bmi = weight / ((height / 100) ** 2);
+    var category = "";
+  
+    if (bmi < 18.5) {
+      category = "Underweight";
+    } else if (bmi < 25) {
+      category = "Normal weight";
+    } else if (bmi < 30) {
+      category = "Overweight";
+    } else {
+      category = "Obese";
+    }
+    resultDiv.innerHTML = "Your BMI is " + bmi.toFixed(2) + " (" + category + ")";
 }
-
-document.getElementById("display").addEventListener('input', function displayChange() {
-    console.log(this.value)
-})
